123a2c9c944dcaf0dde74f3322125003d13ff011
limyeechern <66681646+limyeechern@users.noreply.github.com>
2021-05-20 12:54:26 +0800

    null

--- main.py
+++ main.py
@@ -132,7 +132,7 @@ def course(update: Update, _: CallbackContext) -> int:
     logger.info("Course of %s: %s", user.username, update.message.text.upper())
     newaccount.course = update.message.text.upper()
     update.message.reply_text(
-        'Please indicate the faculty of your first MOD, "FASS" for PL1101E, "Science" for MA1101R, "GE Mods" for '
+        'Please indicate the faculty of your first MOD, e.g. "FASS" for PL1101E, "Science" for MA1101R, "GE Mods" for '
         'GER1000, "Biz" for ACC1002 etc. Please check and input the correct faculty and /cancel whenever you make a '
         'mistake.',
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
